8a08a592c31c54dec10e200cd28b5a1ee6fd7d86
Prian Kuhanandan <priankuhan@gmail.com>
2019-06-02 19:36:24 +0000

    chrome driver fix

--- products/management/commands/get_beers.py
+++ products/management/commands/get_beers.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument("window-size=1024,768")
-        chrome_options.binary_location = GOOGLE_CHROME_BIN
+        # chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN', 'not-a-secret')
         # chrome_options.binary_location = '/usr/bin'
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
