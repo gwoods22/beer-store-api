c39c5b552231792ccbbd08f8d26627593f015c4b
Prian Kuhan <pkuhanandan@wish.com>
2020-03-14 16:52:51 -0400

    lower threshold

--- products/management/commands/delete_prices.py
+++ products/management/commands/delete_prices.py
@@ -4,5 +4,5 @@ from products.models import Product, Price
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-       if Price.objects.count() > 10000:
+       if Price.objects.count() > 8000:
            Price.objects.filter(pk__in=Price.objects.order_by("created_date").values_list('pk')[:4000]).delete()
